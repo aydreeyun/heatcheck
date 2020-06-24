6d24152f95d56ce7e548ad39eecdc081fb7c728f
aydreeyun <uncoadrian@gmail.com>
2020-06-24 14:04:49 -0400

    add photos working

--- src/scripts/graph.js
+++ src/scripts/graph.js
@@ -1,5 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const svg = d3.select('#graph')
+  // const svg = d3.select('#graph')
+  // const button = document.querySelector(".player-button");
 
-  console.log(svg)
+  const sample = [
+    [80, 40, 30, 90, 50, 100],
+    [70, 100, 90, 20, 50, 10, 80]
+  ]
+
+  // console.log(svg)
+  // console.log(svg.height)
+  // console.log(svg.width)
+  // console.log(sample)
+
+  const width = 625, height = 500;
+	const data = [10, 15, 20, 25, 30];
+	
+	const svg = d3.select("#graph")
+		.append("svg")
+		.attr("width", width)
+    .attr("height", height)
+    .style('color', '#f2f2f2')
+
+	const xScale = d3.scaleLinear()
+		.domain([0, d3.max(data)])
+		.range([0, width - 50]);
+
+	const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([height - 50, 0]);
+
+	const xAxis = d3.axisBottom()
+    .scale(xScale)
+    .tickSize(5)
+
+	const yAxis = d3.axisLeft()
+    .scale(yScale)
+    .tickSize(5)
+
+  svg.append("g")
+    .attr("transform", "translate(30, 10)")
+    .call(yAxis);
+
+	const xAxisTranslate = height - 50 + 10;
+
+  svg.append("g")
+    .attr("transform", "translate(30, " + xAxisTranslate  +")")
+    .call(xAxis)
+
+  // button.addEventListener('click', () => {
+  //   console.log("poop")
+  // })
 })
--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,96 +1,117 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // DATA
   const sample = [
-    { name: "bob bobson", pos: "SG", ht: "6'6", pts: "25", reb: "5", ast: "5", stl: "1", blk: "2", to: "3" },
-    { name: "rob robson", pos: "SF", ht: "6'7", pts: "26", reb: "6", ast: "6", stl: "2", blk: "3", to: "2" },
-    { name: "dob dobson", pos: "PF", ht: "6'8", pts: "27", reb: "7", ast: "7", stl: "0", blk: "1", to: "4" }]
+    { name: "bob bobson", pos: "SG", ht: "6'6", pts: "25", reb: "5", ast: "5", stl: "1", blk: "2", to: "3", img: 'src/images/jordan.png' },
+    { name: "rob robson", pos: "SF", ht: "6'7", pts: "26", reb: "6", ast: "6", stl: "2", blk: "3", to: "2", img: 'src/images/harden.png' },
+    { name: "dob dobson", pos: "PF", ht: "6'8", pts: "27", reb: "7", ast: "7", stl: "0", blk: "1", to: "4", img: 'src/images/lebron.png' }
+  ];
 
-  const name = Array.from(document.querySelectorAll(".player-name"))
-  const position = Array.from(document.querySelectorAll(".player-pos"))
-  const height = Array.from(document.querySelectorAll(".player-height"))
-  const points = Array.from(document.querySelectorAll(".points"))
-  const rebounds = Array.from(document.querySelectorAll(".rebounds"))
-  const assists = Array.from(document.querySelectorAll(".assists"))
-  const steals = Array.from(document.querySelectorAll(".steals"))
-  const blocks = Array.from(document.querySelectorAll(".blocks"))
-  const turnovers = Array.from(document.querySelectorAll(".turnovers"))
-  const button = document.querySelector(".player-button");
+  // PLAYER CARD STATS
+  const player = Array.from(document.querySelectorAll('.player-card-details'));
+  const playerImg = Array.from(document.querySelectorAll('.player-photo'));
+
+  const name = Array.from(document.querySelectorAll(".player-name"));
+  const position = Array.from(document.querySelectorAll(".player-pos"));
+  const height = Array.from(document.querySelectorAll(".player-height"));
+  const points = Array.from(document.querySelectorAll(".points"));
+  const rebounds = Array.from(document.querySelectorAll(".rebounds"));
+  const assists = Array.from(document.querySelectorAll(".assists"));
+  const steals = Array.from(document.querySelectorAll(".steals"));
+  const blocks = Array.from(document.querySelectorAll(".blocks"));
+  const turnovers = Array.from(document.querySelectorAll(".turnovers"));
 
   const addStats = (data) => {
-    if (points[0].innerHTML === "PTS:") {
+    if (points[0].innerHTML === "PTS:" && data.name !== name[1].innerHTML) {
       // SET FIRST CARD DATA
-      name[0].innerHTML = `${data[0].name}`
-      position[0].innerHTML = `${data[0].pos}`
-      height[0].innerHTML = `${data[0].ht}`
-      points[0].innerHTML = `PTS: ${data[0].pts}`
-      rebounds[0].innerHTML = `REB: ${data[0].reb}`
-      assists[0].innerHTML = `AST: ${data[0].ast}`
-      steals[0].innerHTML = `STL: ${data[0].stl}`
-      blocks[0].innerHTML = `BLK: ${data[0].blk}`
-      turnovers[0].innerHTML = `TO: ${data[0].to}`
+      name[0].innerHTML = `${data.name}`
+      position[0].innerHTML = `${data.pos}`
+      height[0].innerHTML = `${data.ht}`
+      points[0].innerHTML = `PTS: ${data.pts}`
+      rebounds[0].innerHTML = `REB: ${data.reb}`
+      assists[0].innerHTML = `AST: ${data.ast}`
+      steals[0].innerHTML = `STL: ${data.stl}`
+      blocks[0].innerHTML = `BLK: ${data.blk}`
+      turnovers[0].innerHTML = `TO: ${data.to}`
 
-      closeOne.classList.remove('hidden')
-    } else if (points[1].innerHTML === "PTS:") {
+      // const img = document.query('img')
+      // img.src = data.img
+      // img.id = 'player-1-photo'
+
+      // player[0].appendChild(img)
+      playerImg[0].style.backgroundImage = `url(${data.img})`
+    } else if (points[1].innerHTML === "PTS:" && data.name !== name[0].innerHTML) {
       // SET 2ND CARD DATA
-      name[1].innerHTML = `${data[1].name}`
-      position[1].innerHTML = `${data[1].pos}`
-      height[1].innerHTML = `${data[1].ht}`
-      points[1].innerHTML = `PTS: ${data[1].pts}`
-      rebounds[1].innerHTML = `REB: ${data[1].reb}`
-      assists[1].innerHTML = `AST: ${data[1].ast}`
-      steals[1].innerHTML = `STL: ${data[1].stl}`
-      blocks[1].innerHTML = `BLK: ${data[1].blk}`
-      turnovers[1].innerHTML = `TO: ${data[1].to}`
+      name[1].innerHTML = `${data.name}`
+      position[1].innerHTML = `${data.pos}`
+      height[1].innerHTML = `${data.ht}`
+      points[1].innerHTML = `PTS: ${data.pts}`
+      rebounds[1].innerHTML = `REB: ${data.reb}`
+      assists[1].innerHTML = `AST: ${data.ast}`
+      steals[1].innerHTML = `STL: ${data.stl}`
+      blocks[1].innerHTML = `BLK: ${data.blk}`
+      turnovers[1].innerHTML = `TO: ${data.to}`
 
-      closeTwo.classList.remove('hidden')
-    } else {
-      // REPLACE 2ND CARD DATA
-      name[1].innerHTML = `${data[2].name}`
-      position[1].innerHTML = `${data[2].pos}`
-      height[1].innerHTML = `${data[2].ht}`
-      points[1].innerHTML = `PTS: ${data[2].pts}`
-      rebounds[1].innerHTML = `REB: ${data[2].reb}`
-      assists[1].innerHTML = `AST: ${data[2].ast}`
-      steals[1].innerHTML = `STL: ${data[2].stl}`
-      blocks[1].innerHTML = `BLK: ${data[2].blk}`
-      turnovers[1].innerHTML = `TO: ${data[2].to}`
-    }
+      // const img = document.createElement('img')
+      // img.src = data.img
+      // img.id = 'player-2-photo'
+
+      // player[1].appendChild(img)
+      playerImg[1].style.backgroundImage = `url(${data.img})`
+    } 
+    // else {
+    //   // REPLACE 2ND CARD DATA
+    //   if (name[0].innerHTML !== data.name) {
+    //     name[1].innerHTML = `${data.name}`
+    //     position[1].innerHTML = `${data.pos}`
+    //     height[1].innerHTML = `${data.ht}`
+    //     points[1].innerHTML = `PTS: ${data.pts}`
+    //     rebounds[1].innerHTML = `REB: ${data.reb}`
+    //     assists[1].innerHTML = `AST: ${data.ast}`
+    //     steals[1].innerHTML = `STL: ${data.stl}`
+    //     blocks[1].innerHTML = `BLK: ${data.blk}`
+    //     turnovers[1].innerHTML = `TO: ${data.to}`
+    //   }
+    // }
+  };
+
+  const closeStats = (idx) => {
+    name[idx].innerHTML = ""
+    position[idx].innerHTML = ""
+    height[idx].innerHTML = ""
+    points[idx].innerHTML = "PTS:"
+    rebounds[idx].innerHTML = "REB:"
+    assists[idx].innerHTML = "AST:"
+    steals[idx].innerHTML = "STL:"
+    blocks[idx].innerHTML = "BLK:"
+    turnovers[idx].innerHTML = "TO:"
+
+    playerImg[idx].style.backgroundImage = ""
+    // const playerImg = document.getElementById(`player-${idx + 1}-photo`)
+    // player[idx].removeChild(playerImg)
   }
 
-  button.addEventListener("click", () => {
-    addStats(sample);
-  })
+  // PLAYER BUTTONS
+  const buttons = Array.from(document.querySelectorAll('.player-button'));
 
-  // CLOSE BUTTONS
-  const closeOne = document.querySelector('.close-one')
-  const closeTwo = document.querySelector('.close-two')
+  buttons.forEach(button => {
+    const idx = sample.indexOf(sample.find(el => el.name === button.innerHTML))
 
-  closeOne.addEventListener('click', () => {
-    name[0].innerHTML = ""
-    position[0].innerHTML = ""
-    height[0].innerHTML = ""
-    points[0].innerHTML = "PTS:"
-    rebounds[0].innerHTML = "REB:"
-    assists[0].innerHTML = "AST:"
-    steals[0].innerHTML = "STL:"
-    blocks[0].innerHTML = "BLK:"
-    turnovers[0].innerHTML = "TO:"
-
-    closeOne.classList.add('hidden')
-  })
-
-  closeTwo.addEventListener('click', () => {
-    name[1].innerHTML = ""
-    position[1].innerHTML = ""
-    height[1].innerHTML = ""
-    points[1].innerHTML = "PTS:"
-    rebounds[1].innerHTML = "REB:"
-    assists[1].innerHTML = "AST:"
-    steals[1].innerHTML = "STL:"
-    blocks[1].innerHTML = "BLK:"
-    turnovers[1].innerHTML = "TO:"
-
-    closeTwo.classList.add('hidden')
-  })
+    button.addEventListener("click", () => {
+      if (name[0].innerHTML  === sample[idx].name) {
+        closeStats(0);
+        button.classList.remove('active');
+      } else if (name[1].innerHTML  === sample[idx].name) {
+        closeStats(1);
+        button.classList.remove('active');
+      } else {
+        if (!name[0].innerHTML || !name[1].innerHTML) {
+          button.classList.add('active');
+        }
+  
+        addStats(sample[idx]);
+      }
+    });
+  });
 
   // d3.csv("src/data/career.csv")
   //   .then(data => {
@@ -98,5 +119,5 @@ document.addEventListener('DOMContentLoaded', () => {
   //     d3.select(".player-select")
   //       .append('button')
   //       .text("click me")
-  //   })
+  //   });
 })
