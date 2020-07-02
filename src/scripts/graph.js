0ea4f58ecfb6b0779215f35242b89105c28bd033
aydreeyun <uncoadrian@gmail.com>
2020-07-01 23:25:53 -0400

    fixed graph rerender - less seasons

--- src/scripts/graph.js
+++ src/scripts/graph.js
@@ -14,61 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
     .append('g')
     .attr('transform', 'translate(' + margin.bottom + ',' + margin.top + ')')
 
-  // const drawXAxis = data => {
-  //   const xValues = data.map(obj => obj.season)
-
-  //   if (xValues.length > currXAxisVal) {
-  //     currXAxisVal = xValues.length;
-  //     currXAxis = xValues;
-  //   }
-
-  //   const xScale = d3.scalePoint()
-  //     .domain(currXAxis)
-  //     .range([0, graphWidth])
-
-  //   svg.append("g")
-  //     .attr("transform", "translate(0," + graphHeight + ")")
-  //     .attr('class', 'axis x-axis')
-  //     .call(d3.axisBottom(xScale));
-  // }
-
-  // const drawYAxis = stat => {
-  //   const yScale = d3.scaleLinear()
-  //     .range([graphHeight, 0])
-
-  //   if (stat === "pts") {
-  //     yScale.domain([0, 50])
-
-  //     svg.append("g")
-  //       .attr('class', 'axis y-axis')
-  //       .call(d3.axisLeft(yScale).tickValues([0, 10, 20, 30, 40, 50]));
-  //   } else if (stat === "reb" || stat === "ast") {
-  //     yScale.domain([0, 15])
-
-  //     svg.append("g")
-  //       .attr('class', 'axis y-axis')
-  //       .call(d3.axisLeft(yScale).tickValues([0, 3, 6, 9, 12, 15]));
-  //   } else if (stat === "stl" || stat === "blk") {
-  //     yScale.domain([0, 4])
-
-  //     svg.append("g")
-  //       .attr('class', 'axis y-axis')
-  //       .call(d3.axisLeft(yScale).tickValues([0, 1, 2, 3, 4]));
-  //   } else if (stat === "to") {
-  //     yScale.domain([0, 6])
-
-  //     svg.append("g")
-  //       .attr('class', 'axis y-axis')
-  //       .call(d3.axisLeft(yScale).tickValues([0, 2, 4, 6]));
-  //   } else if (stat === "fg%" || stat === "3p%" || stat === "ft%") {
-  //     yScale.domain([0, 1])
-
-  //     svg.append("g")
-  //       .attr('class', 'axis y-axis')
-  //       .call(d3.axisLeft(yScale).tickValues([0, 0.2, 0.4, 0.6, 0.8, 1.0]));
-  //   }
-  // }
-
   // DRAW LINE
   let currXAxisVal = 0;
   let currXAxis;
@@ -85,9 +30,17 @@ document.addEventListener('DOMContentLoaded', () => {
         
         // X-AXIS
         const xValues = parsedData.map(obj => obj.season)
+
         if (xValues.length > currXAxisVal) {
           currXAxisVal = xValues.length;
           currXAxis = xValues;
+
+          if (Object.keys(currPlayers).length === 2) {
+            const firstPlayer = Object.keys(currPlayers)[0];
+            const firstPlayerName = firstPlayer.split(" ")[1].toLowerCase();
+  
+            redrawLine(firstPlayerName, stat, currPlayers[firstPlayer]);
+          }
         }
 
         const xScale = d3.scalePoint()
@@ -160,6 +113,58 @@ document.addEventListener('DOMContentLoaded', () => {
       })
   }
 
+  const redrawLine = (name, stat, color) => {
+    svg.selectAll(`path.${name}`).remove();
+    svg.selectAll(`circle.${name}`).remove();
+
+    d3.csv(`src/data/${name}.csv`)
+      .then(data => {
+        const parsedData = parseData(data, stat);
+
+        const xScale = d3.scalePoint()
+        .domain(currXAxis)
+        .range([0, graphWidth])
+
+        const yScale = d3.scaleLinear()
+        .range([graphHeight, 0])
+
+        if (stat === "pts") {
+          yScale.domain([0, 50])
+        } else if (stat === "reb" || stat === "ast") {
+          yScale.domain([0, 15])
+        } else if (stat === "stl" || stat === "blk") {
+          yScale.domain([0, 4])
+        } else if (stat === "to") {
+          yScale.domain([0, 6])
+        } else if (stat === "fg%" || stat === "3p%" || stat === "ft%") {
+          yScale.domain([0, 1])
+        }
+
+        svg.append("path")
+        .datum(parsedData)
+        .attr("fill", "none")
+        .attr("stroke", `${color}`)
+        .attr("stroke-width", 1.5)
+        .attr('class', `line ${name}`)
+        .attr("d", d3.line()
+          .x(function(d) { return xScale(d.season) })
+          .y(function(d) { return yScale(d[stat]) })
+        )
+
+        // ADD CIRCLE
+        svg.append("g")
+          .selectAll("dot")
+          .data(parsedData)
+          .enter()
+          .append("circle")
+          .attr('class', `dot ${name}`)
+          .attr("cx", function(d) { return xScale(d.season) } )
+          .attr("cy", function(d) { return yScale(d[stat]) } )
+          .attr("r", 2.5)
+          .attr("fill", `${color}`)
+      })
+  }
+
   // PARSE DATA
   const parseData = (data, stat) => {
     const arr = [];
@@ -185,7 +190,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // PLAYER BUTTONS
-  const playerButtons = document.querySelectorAll('.player-button')
+  const playerButtons = Array.from(document.querySelectorAll('.player-button'));
   const colors = { red: false, dodgerblue: false };
 
   playerButtons.forEach(button => {
